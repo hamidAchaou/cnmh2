09bfa6077d9bc06a80388b5311ecaeff10bf7ca3
boukharsoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-11-15 18:56:14 +0100

    Ajouter Repositories

---
+++ app/Repositories/ConsultationRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Consultation;
+use App\Repositories\BaseRepository;
+
+class ConsultationRepository extends BaseRepository
+{
+    protected $fieldSearchable = [
+        'date_enregistrement',
+        'date_consultation',
+        'observation',
+        'diagnostic',
+        'type',
+        'bilan'
+    ];
+
+    public function getFieldsSearchable(): array
+    {
+        return $this->fieldSearchable;
+    }
+
+    public function model(): string
+    {
+        return Consultation::class;
+    }
+}
