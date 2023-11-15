5f285b1e89d2ef0906e5fcca19c6828b61c65810
boukharsoufiane1998 <boukhar.soufiane.coding@gmail.com>
2023-11-15 18:34:19 +0100

    Ajouter middleware

---
+++ app/Http/Middleware/ModelExists.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ModelExists
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        //Get params
+        $params = $request->model;
+        //Define model
+        $model = 'App\\Models\\' . ucfirst($params);
+        //check if subclass model
+        if (is_subclass_of($model, 'App\\Models\\Consultation')) {
+
+            return $next($request);
+
+        }if(ucfirst($request->model) == "Liste-attente"){
+
+            return $next($request);
+
+        } else {
+            abort(404);
+        }
+    }
+}
